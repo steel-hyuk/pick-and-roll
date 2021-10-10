41d6d9958021080909e861fbb294a3730518dd25
youhavetosleep <qksekf4@gmail.com>
2021-10-10 17:32:40 +0900

    Fix conflict2

--- client/src/component/editMyInfo/showInfoComponent.jsx
+++ client/src/component/editMyInfo/showInfoComponent.jsx
@@ -1,6 +1,6 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
-import {UserContext} from '../../context/userContext'
+import { UserContext } from '../../context/userContext'
 
 const ShowInfoComponent = () => {
   const { userInfo, setUserInfo } = useContext(UserContext)
