cc5dd22b36ac1f2b53d0034d1c47a4111a96f1e6
wonjae jun <woncha7764@gmail.com>
2021-10-11 23:51:15 +0900

    WriteComponent, ImgEncoding folder 수정 및 추가 - firebase storage를 사용한 img encoding으로 전환 firebase config 추가 및 부수적인 린트 수정

---
+++ client/src/component/ImgEncoding/firebase.jsx
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getStorage, ref } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+}
+
+const firebase = initializeApp(firebaseConfig)
+const analytics = getAnalytics(firebase)
+
+export const storageService = getStorage(firebase)
+
+export const storageRef = ref(storageService)
+
+export default firebase
