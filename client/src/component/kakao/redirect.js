0b7342665a4584752baaf08978d14649dbe27ca1
jsjsjskjs <jsjsjskjs@gmail.com>
2021-10-12 10:40:59 +0900

    console.log remove

--- client/src/component/kakao/redirect.js
+++ client/src/component/kakao/redirect.js
@@ -3,10 +3,9 @@ import axios from 'axios'
 
 const Redirect = (props) => {
   let code = new URL(window.location.href).searchParams.get('code')
-  console.log('여기서 확인이 되는가??', code)
   axios.get(`http://localhost:80/users/signup/oauth/kakao?code=${code}`)
     .then((res) => {
-      console.log('gagaga')
+      console.log('카카오톡 로그인 확인 중')
     })
     .catch(err => {
       console.log(err.message)
--- client/src/page/info.jsx
+++ client/src/page/info.jsx
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 
 const Info = () => {
-  let code = new URL(window.location.href).searchParams.get("code");
-  console.log(code)
   return <h1>Info</h1>
 }
 export default Info
